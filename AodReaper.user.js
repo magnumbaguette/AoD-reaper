eaa644502e2fb689da78025502c8ebd875667cf7
magnumbaguette <blue01claws@gmail.com>
2018-11-30 20:11:50 +0530

    More refactoring

--- AodReaper.user.js
+++ AodReaper.user.js
@@ -23,29 +23,35 @@
     styler.href="https://cdn.jsdelivr.net/gh/magnumbaguette/notes/test.css"
     document.getElementsByTagName('head')[0].appendChild(styler);
 
-    //define the aod menu section
+
+
+    //define the aod news section
+    var para = document.createElement('h1');
+    var aod_logo = document.createElement('img');
+    var trr = document.createElement('th');
+    aod_logo.src = "https://i.imgur.com/aWlyTRx.png";
+    aod_logo.setAttribute('class', 'center');
+    para.textContent = `Welcome to your Reaper account(alpha 0.0.1)`;
+    para.style.color = 'red';
+    para.style.textAlign = 'center'
+    trr.appendChild(para);
+    trr.appendChild(aod_logo);
+
+    //define aod menu section.
     var menu = document.getElementsByTagName('tr')[8];
     var menu = menu.cloneNode(true); // ignore error here in the userscript viewer
     menu.querySelectorAll('img')[0].src='https://i.imgur.com/HKZo0sT.png';
     var childOfMenuParent = document.getElementsByTagName('tbody')[3].querySelectorAll('tr')[1];
-    document.getElementsByTagName('tbody')[3].insertBefore(menu, childOfMenuParent);
 
-    //define the aod news section.
+
+    //Page definitions.
     function AoDNews(){
-        var para = document.createElement('h1');
-        var aod_logo = document.createElement('img');
-        aod_logo.src = "https://i.imgur.com/aWlyTRx.png";
-        aod_logo.setAttribute('class', 'center');
-        para.textContent = `Welcome to your Reaper account(alpha 0.0.1)`;
-        para.style.color = 'red';
-        para.style.textAlign = 'center'
-        var trr = document.createElement('th');
-        trr.appendChild(para);
-        trr.appendChild(aod_logo);
         document.querySelectorAll('table')[6].querySelector('tbody').appendChild(para);
         document.querySelectorAll('table')[6].querySelector('tbody').appendChild(aod_logo);
+        document.getElementsByTagName('tbody')[3].insertBefore(menu, childOfMenuParent);
     }
 
+    //Caller conditions.
     if( window.location.href == 'https://www.kingsofchaos.com/base.php'){
         AoDNews();
     }
