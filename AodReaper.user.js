82b9676737260592170b273829a11c506bc070f4
magnumbaguette <blue01claws@gmail.com>
2018-12-11 17:30:59 +0530

    Updated with better text logging.

--- AodReaper.user.js
+++ AodReaper.user.js
@@ -24,9 +24,18 @@
     var USER = GM_getValue("USER");
     var ID = GM_getValue("ID");
     var UID = GM_getValue("UID");
-    var REAPER_VERSION = `0.0.3`;
+    var REAPER_VERSION = `0.0.4`;
+    versionCheck();
 
-   // GM_deleteValue("PASS");
+    var reaper_user = GetText("b>Name</b", "\">", "<");
+    var reaper_id = GetText("b>Name</b", "id=", "\"");
+    var reaper_uid = GetText("uniqid=", "\"");
+
+    GM_setValue("USER", reaper_user);
+    GM_setValue("ID", reaper_id);
+    GM_setValue("UID", reaper_uid);
+
+   //GM_deleteValue("PASS");
     console.log(GM_getValue("PASS"));
    //alert(GM_getValue("PASS"));
     ///// THIS FUNCTION NEEDS BETTER AUTH MECHANISM IN PRODUCTION /////
@@ -39,13 +48,36 @@
         method: "GET",
         url: SERVER + "register/" + USER + "/" + ID + "/" + UID + '/' + PASS,
         onload: function(response) {
-                 document.querySelectorAll('center')[2].querySelectorAll('img')[0].src = "https://cdn.frankerfacez.com/emoticon/219766/4";
-                 document.querySelectorAll('center')[2].removeChild(document.querySelectorAll('sub')[0]);
+                console.log(response);
                  document.querySelectorAll('h1')[0].innerText = "You are registered! Please wait for an admin to approve your account.";
                  document.querySelectorAll('center')[3].removeChild(document.querySelectorAll('button')[0]);
         }
     });
+   }
+
+    function ErrorCorrection(){
+            PASS = prompt("Enter the password: ");
+            GM_setValue("PASS", PASS);
     }
+
+    function Wait(){
+                 var oopsies = document.createElement('div');
+                 document.querySelectorAll('table')[6].querySelector('tbody').appendChild(oopsies);
+                 oopsies.innerHTML = `<center><br><h1 style="color: #F4900C">You are registered! Please wait for an admin to approve your account.</h1></center>`;
+    }
+
+    function Error(){
+                 var oopsies = document.createElement('div');
+                 document.querySelectorAll('table')[6].querySelector('tbody').appendChild(oopsies);
+                 oopsies.innerHTML = `<center><br><h1 style="color: #F4900C">You've entered wrong credentials.</h1></center>`;
+                 var centerer = document.createElement('center');
+                 var registerbutton = document.createElement('button');
+                 centerer.appendChild(registerbutton);
+                 registerbutton.innerHTML = `<font size="5" color="#FFCC4D">Login</font>`;
+                 document.querySelectorAll('table')[6].querySelector('tbody').appendChild(centerer);
+                 registerbutton.addEventListener("click", ErrorCorrection);
+    }
+
     // define the css link from the repo
     var sty = document.createElement('style');
    sty.innerHTML = `
@@ -67,19 +99,11 @@
     }
 
     if( window.location.href == 'https://www.kingsofchaos.com/base.php'){
-        var reaper_user = document.querySelectorAll('a')[29].innerHTML;
-        var reaper_id = document.querySelectorAll('a')[29].href.slice(-7);
-        var reaper_uid = document.querySelectorAll('a')[27].href.slice(-7);
         if(PASS == undefined || PASS == null){
             PASS = prompt("Enter the password: ");
             GM_setValue("PASS", PASS);
         }
         //alert(PASS);
-
-        GM_setValue("USER", reaper_user);
-        GM_setValue("ID", reaper_id);
-        GM_setValue("UID", reaper_uid);
-
         GM_xmlhttpRequest({
         method: "GET",
         url: SERVER + "info/" + USER + "/" + ID + "/" + UID + '/' + PASS,
@@ -88,9 +112,7 @@
              if(response.responseText == "no"){
                  var oopsies = document.createElement('div');
                  document.querySelectorAll('table')[6].querySelector('tbody').appendChild(oopsies);
-                 oopsies.innerHTML = `<center><img src="https://cdn.frankerfacez.com/emoticon/292440/4" alt="Thonking">
-                                     <br><sub>refresh page if you registered</sub>
-                                    <br><h1 style="color: #F4900C">Reaper did some THONKING and it says you are not registered.</h1></center>`;
+                 oopsies.innerHTML = `<center><br><h1 style="color: #F4900C">Reaper did some THONKING and it says you are not registered.</h1></center>`;
                  var centerer = document.createElement('center');
                  var registerbutton = document.createElement('button');
                  centerer.appendChild(registerbutton);
@@ -99,7 +121,14 @@
                  registerbutton.addEventListener("click", Register);
                  //console.log(owo);
              }
-            else if(response.responseText == "yes"){
+            else if(response.responseText == "wait"){
+                //alert("wating");
+                Wait();
+            }
+            else if(response.reponseText == "error"){
+                Error();
+            }
+            else if(response.responseText =="yes"){
                 AoDNews();
             }
         }
@@ -157,7 +186,38 @@
 
     }
 
-
+console.log(reaper_user, reaper_id, reaper_uid);
+    function versionCheck(){
+        GM_xmlhttpRequest({
+        method: "GET",
+        url: SERVER + "version/" + REAPER_VERSION,
+        onload: function(response) {
+            if(response.responseText == "outdated"){
+                alert("You are using an outdated version of Reaper, please update.")
+            }
+        }
+    });
+    }
 ///////// END CUSTOM PAGE /////////
+
+function GetText()
+{
+    var doc = document.body.innerHTML;
+
+    var pos = 0;
+
+    for(var z = 0; z < (arguments.length - 1); z++)
+    {
+        pos = doc.indexOf(arguments[z], pos);
+        if(pos < 0) return "";
+
+        pos += arguments[z].length;
+    }
+
+    var pos2 = doc.indexOf(arguments[arguments.length - 1], pos);
+    if(pos2 < 0) return "";
+
+    return doc.substring(pos, pos2);
+}
 
 })();
